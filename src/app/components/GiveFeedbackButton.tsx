0b5966ef271816eaab20ebdcb23b540eebabc324
GenDelta <ankush.dutta.kol@gmail.com>
2025-04-20 14:00:24 +0530

    refactor: update GiveFeedbackButton component and edit styling of Faculty Page

--- src/app/components/GiveFeedbackButton.tsx
+++ src/app/components/GiveFeedbackButton.tsx
@@ -4,7 +4,11 @@ import React from "react";
 import { useRouter } from "next/navigation";
 
 interface GiveFeedbackButtonProps {
-  context: "Faculty Feedback" | "Guest Feedback" | "Curriculum Feedback" | "Open Dashboard";
+  context:
+    | "Faculty Feedback"
+    | "Guest Feedback"
+    | "Curriculum Feedback"
+    | "Open Dashboard";
 }
 
 const GiveFeedbackButton: React.FC<GiveFeedbackButtonProps> = ({ context }) => {
@@ -30,12 +34,16 @@ const GiveFeedbackButton: React.FC<GiveFeedbackButtonProps> = ({ context }) => {
   };
 
   return (
-    <button
-      className="bg-gradient-to-r from-buttons-gradientlight to-buttons-gradientdark text-white rounded-lg p-2 opacity-100"
+    <div
+      className="relative inline-block group cursor-pointer"
       onClick={handleClick}
     >
-      {context === "Open Dashboard" ? "Open Dashboard" : "Give Feedback"}
-    </button>
+      <div className="relative text-white rounded-lg p-2 z-10">
+        {context === "Open Dashboard" ? "Open Dashboard" : "Give Feedback"}
+      </div>
+      <div className="absolute inset-0 bg-gradient-to-r from-buttons-gradientlight to-buttons-gradientdark rounded-lg z-0"></div>
+      <div className="absolute inset-0 bg-gradient-to-l from-buttons-gradientlight to-buttons-gradientdark rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-[1000ms] ease-in-out z-0"></div>
+    </div>
   );
 };
 
