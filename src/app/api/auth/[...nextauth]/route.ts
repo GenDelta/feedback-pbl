bbb61929ae15e6d1874d02871a62819f8e409a43
Mitiksha Paliwal <paliwalmitiksha@gmail.com>
2025-04-22 00:11:58 +0530

    schema update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,11 @@
+import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
+  session: { strategy: "jwt" },
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
