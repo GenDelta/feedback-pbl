a8c6ea46379af64cc06eb081149b6effa42e3175
Mitiksha Paliwal <paliwalmitiksha@gmail.com>
2025-05-01 16:02:13 +0530

    refactor: Update LoginFooter

--- src/app/components/LoginFooter.tsx
+++ src/app/components/LoginFooter.tsx
@@ -1,24 +1,32 @@
-import React from "react";
+"use client";
 
-const LoginFooter = () => {
+import React from "react";
+import { useRouter } from "next/navigation";
+
+const Footer = () => {
+  const router = useRouter();
+
   return (
-    <div className="bg-gray-800 h-14 w-svw">
-      <hr className="absolute top-3 w-svw border-gray-200" />
+    <div className="bg-gray-800 h-14 w-svw fixed bottom-0">
+      <hr className="w-svw border-gray-200" />
       <div className="grid grid-cols-3 items-center pt-4">
-        <div className="text-gray-200 text-left font-poppins pl-4">
+        <div className="text-gray-200 text-left font-poppins pl-4 ml-6">
           Feedback | Copyright © 2025
         </div>
-        <div className="text-gray-200 text-center font-poppins">
+        <div className="text-gray-200 text-center font-poppins ml-6">
           Ideation by Dr.Deepali Vora, Head CS IT
         </div>
-        <div className="text-gray-200 text-right font-poppins">
-          <div>
-            Redesigned by Team: Ankush Dutta, Mitiksha Paliwal, Tanvee Patil
-          </div>
+        <div className="text-gray-200 text-right font-poppins mr-6">
+          <button
+            className="text-blue-500 bg-transparent"
+            onClick={() => router.push("/team")}
+          >
+            Meet the team
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-export default LoginFooter;
+export default Footer;
